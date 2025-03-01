05d88442799ce5638669238dfa87dc1e8dd8e358
CharanTeja-6825 <rcharanteja2006@gmail.com>
2025-03-01 21:57:07 +0530

    Navbar css

--- asthetica/src/navbar/Navbar.jsx
+++ asthetica/src/navbar/Navbar.jsx
@@ -12,17 +12,25 @@ import Profile from '../components/Profile';
 import { IoSearchSharp } from "react-icons/io5";
 import { IoMdHeart } from "react-icons/io";
 import { IoPersonSharp } from "react-icons/io5";
-
+import './Navbar.css'
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <Link to='/'>Asthetica</Link>
-            <Link to='/'>Home</Link>
-            <Link to='artists'>Artists</Link>
-            <Link to='autions'>Auctions</Link>
-            <Link to='search'><IoSearchSharp /></Link>
-            <Link to='wishlist'><IoMdHeart /></Link>
-            <Link to='profile'><IoPersonSharp/></Link>
+            <div className='nav-links'>
+                <div className='nav-start-section'>
+                    <Link to='/'><strong>Asthetica</strong></Link>
+                </div>
+                <div className='nav-mid-section'>
+                    <Link to='/'>Home</Link>
+                    <Link to='artists'>Artists</Link>
+                    <Link to='autions'>Auctions</Link>
+                </div>
+                <div className='nav-end-section'>
+                    <Link to='search'><IoSearchSharp /></Link>
+                    <Link to='wishlist'><IoMdHeart /></Link>
+                    <Link to='profile'><IoPersonSharp/></Link>
+                </div>
+            </div>
             
             <Routes>
                 <Route path='/' element={<Home/>}/>
