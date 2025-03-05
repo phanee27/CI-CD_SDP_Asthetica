2a508298c46c7abcc86e3f74fab9a10b33b53208
CharanTeja-6825 <rcharanteja2006@gmail.com>
2025-03-05 13:51:39 +0530

    reg complete

--- asthetica/src/underdev/Registration.jsx
+++ asthetica/src/underdev/Registration.jsx
@@ -11,20 +11,21 @@ const Registration = () => {
             <p className='register-quote'>start your journey collecting stunning art</p>
             <div className='register-section'>
                 <div className='register-input-fields'>
+                    <form action="">
                     <label htmlFor="fullname">Full Name</label><br />
-                    <input type="text" id='fullname' placeholder='Enter full name'/><br />
+                    <input type="text" id='fullname' placeholder='Enter full name' required/><br />
                     <label htmlFor="fullname">Email</label><br />
-                    <input type="email" id='email' placeholder='Enter email'/><br />
+                    <input type="email" id='email' placeholder='Enter email' required/><br />
                     <label htmlFor="pwd">Password</label><br />
-                    <input type="password" id='pwd' placeholder='Enter password'/><br />
+                    <input type="password" id='pwd' placeholder='Enter password' required/><br />
                     <label htmlFor="con-pwd">Confirm Password</label><br />
-                    <input type="password" id='con-pwd' placeholder='confirm password'/><br />
-                </div>
-                <div className='register-create'>
-                    <button>Create Account</button>
+                    <input type="password" id='con-pwd' placeholder='confirm password' required/><br />
+                    <button className='register-create' type='submit'>Submit</button>
+                    </form>
+                    <p className='register-sign-in'>already have an account? <Link to='/login'>Login here</Link></p>
                 </div>
             </div>
-            <p className='register-sign-in'>already have an account? <Link to='/login'>Login here</Link></p>
+           
         </div>
     );
 }
