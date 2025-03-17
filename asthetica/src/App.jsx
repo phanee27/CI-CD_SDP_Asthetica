168742db254f252ed44fa5ea7a4c68e28236cc19
CharanTeja-6825 <rcharanteja2006@gmail.com>
2025-03-17 22:14:34 +0530

    minute mods

--- asthetica/src/App.jsx
+++ asthetica/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
 
     <div>
       <BrowserRouter>
-        {/* <AdminNavbar/> */}
-        <SellerNavbar/>
+        <AdminNavbar/>
+        {/* <SellerNavbar/> */}
         {/* <CustomerNavbar/> */}
         <Footer/>
       </BrowserRouter>
