81b1a73973abc29bc8f0baf9c74d29981c0cee80
isuryaprakashh <isurya2005@gmail.com>
2025-04-08 16:57:32 +0530

    updated the css of registration

--- FRONTEND/src/authentication/Registration.jsx
+++ FRONTEND/src/authentication/Registration.jsx
@@ -52,10 +52,13 @@ const Registration = () => {
     }
     return (
         <div className='register'>
-            <p className='register-header' style={{fontSize:'larger'}}>ASTHETICA</p>
+            <p className='register-header' style={{fontSize:'larger', fontWeight:'bold'}}>ASTHETICA</p>
+            
+            <div className='register-section' style={{justifyContent:"center"}}>
+            <div className="header">
             <h1>Create Account</h1>
             <p className='register-quote'>start your journey collecting stunning art</p>
-            <div className='register-section'>
+            </div>
                 <div className='register-input-fields'>
                 {
                     message ? (
@@ -69,22 +72,22 @@ const Registration = () => {
                     ) : null
                 }
                     <form onSubmit={handleSubmit}>
-                    <label htmlFor="fullname">Full Name</label><br />
+                    <label htmlFor="fullname">Full Name</label>
                     <input type="text" id='name' value={formData.name} onChange={handleChange} placeholder='Enter full name' required/><br />
-                    <label htmlFor="gender"  >Gender:</label><br />
+                    <label htmlFor="gender"  >Gender:</label>
                         <select id="gender" name="gender" value={formData.gender} onChange={handleChange} style={{width:"100%"}}>
                         <option value="" disabled>-- select --</option>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                         <option value="Other">Other</option>
                         </select><br />
-                    <label htmlFor="fullname" >Email</label><br />
+                    <label htmlFor="fullname" >Email</label>
                     <input type="email" id='email' value={formData.email} onChange={handleChange} placeholder='Enter email' required/><br />
-                    <label htmlFor='username'>Username</label><br/>
-                    <input type="text" id='username' value={formData.username} onChange={handleChange} placeholder='Enter username' required/><br/>
-                    <label htmlFor="pwd">Password</label><br />
+                    <label htmlFor='username'>Username</label>
+                    <input type="text" id='username' value={formData.username} onChange={handleChange} placeholder='Enter username' required/><br />
+                    <label htmlFor="pwd">Password</label>
                     <input type="password" id='password' value={formData.password} onChange={handleChange} placeholder='Enter password' required/><br />
-                    <label htmlFor="contact">Contact</label><br/>
+                    <label htmlFor="contact">Contact</label>
                     <input type="number" id='contact' value={formData.contact} onChange={handleChange} placeholder='Enter contact' required/><br />
                     <button className='register-create' type='submit'>Submit</button>
                     </form>
