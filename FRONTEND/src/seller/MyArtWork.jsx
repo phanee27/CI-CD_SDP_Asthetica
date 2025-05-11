7f11c0deae3089325d966ca9b3fb09ac95425703
isuryaprakashh <isurya2005@gmail.com>
2025-05-11 06:26:32 +0530

    updated src/seller/myartwork import stataement again 2

--- FRONTEND/src/seller/MyArtWork.jsx
+++ FRONTEND/src/seller/MyArtWork.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import config from "../../config";
 import './styles/MyArtWork.css';
-import ArtworkCard from "./ArtworkCard";
+// import ArtworkCard from "./ArtworkCard";
 
 
 const MyArtWork = () => {
